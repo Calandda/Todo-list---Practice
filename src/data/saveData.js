569e354384765fa914abae93187b377d5cb19e6b
Calandda <A.calandda@gmail.com>
2025-01-27 23:21:17 +0800

    Added function for module of saveData

--- src/data/saveData.js
+++ src/data/saveData.js
@@ -1,12 +1,41 @@
+class projectObject{
+    constructor(title,projectName,description,dueDate,priority,notes,check){
+        this.title = title;
+        this.projectName = projectName;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.check = check;
+    }
+}
 class saveDataObject{
+    projects = [];
+
     constructor(){
+        this.createProject('default Project');
     }
     classTest(){
         console.log('saveData confirmed');
     }
     createProject(projectName){
+        let tempObject = {};
+        tempObject.projectName = projectName;
+        tempObject.projectTodoList = [];
+        this.projects.push(tempObject);
     };
-}
+    createProjectList(title,projectName,description,dueDate,priority,notes,check){
+        const tempObject = new projectObject(title,projectName,description,dueDate,priority,notes,check); 
+        for(let i = 0; i < this.projects.length;i++){
+            if(this.projects[i].projectName === projectName){
+                this.projects[i].projectTodoList.push(tempObject);
+            };
+        }
+    };
+    checkProject(){
+        console.log(this.projects);
+    };
+};
 
 const saveData = new saveDataObject();
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import saveData from './data/saveData.js';
 
 console.log(saveData.classTest());
+saveData.createProjectList('Project Test','default Project','Project Description','2025','1','Notes','CHECK');
+saveData.createProject('Daily');
+saveData.createProjectList('Project2 Test','Daily','Project2 Description','2025','1','Notes','CHECK');
+saveData.createProjectList('Project2 Test2','Daily', 'Project2 Description', '2025', '2','Notes 2', 'CHECK');
+console.log(saveData.checkProject());
+//title,description,dueDate,priority,notes,check
