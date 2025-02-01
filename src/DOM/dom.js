abb5bc6fe393e92b1eb605dc3969007f49660bf4
Calandda <A.calandda@gmail.com>
2025-02-01 19:47:24 +0800

    Added Project List in DOM/HTML

--- src/DOM/dom.js
+++ src/DOM/dom.js
@@ -1,13 +1,31 @@
 class domChangeObject{
-    constructor(project){
-        this.fillProject(project);
-        this.fillTodoList(project);
+    constructor(projects){
+        this.sectionProjectList = document.querySelector('.sectionProject');
+        this.fillProject(projects);
+        this.fillTodoList(projects[0]);
     };
-    fillProject(project){
-        console.log(project);
+    fillProject(projects){
+        const PROJECT_LENGTH = projects.length;
+        for(let i = 0;i< PROJECT_LENGTH;i++){
+            this.fillProjectIndividual(projects[i]);
+        };
     };
     fillTodoList(project){
         console.log(project);
+    };
+    fillProjectIndividual(project){
+        const divProject = document.createElement('div');
+        const pProjectTitle = document.createElement('p');
+        const pTodoLength = document.createElement('p');
+        console.log(project.projectName);
+        pProjectTitle.textContent = project.projectName;
+        divProject.classList.add('divProjectList');
+        pTodoLength.classList.add('pTodoCount');
+        pTodoLength.classList.add('bgColorDarkGray');
+        pTodoLength.textContent = project.projectTodoList.length;
+        divProject.appendChild(pProjectTitle);
+        divProject.appendChild(pTodoLength);
+        this.sectionProjectList.appendChild(divProject);
     };
 }
 
